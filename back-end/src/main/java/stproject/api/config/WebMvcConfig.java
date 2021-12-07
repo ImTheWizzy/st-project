75ef0701983a0b9b835a667529ef86ed92153831
ViktorGG <viktor.grgv47@gmail.com>
2021-12-07 14:18:52 +0200

    Fixed registration post controller

--- back-end/src/main/java/stproject/api/config/WebMvcConfig.java
+++ back-end/src/main/java/stproject/api/config/WebMvcConfig.java
@@ -2,11 +2,12 @@ package stproject.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Bean
