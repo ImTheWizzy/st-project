a558228ab5cb7099a55afdfb2a330acb89320db9
vikittyy99 <vikittyy99_99@abv.bg>
2021-12-08 11:37:55 +0200

    add patient

--- front-end/src/RootRoutes.tsx
+++ front-end/src/RootRoutes.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./screens/HomePage";
 import SignIn from "./screens/SignIn";
 import SignUp from "./screens/SignUp";
-
+import Patient from "./screens/Patient";
 export default function RootRoutes() {
   return (
     <>
@@ -11,6 +11,7 @@ export default function RootRoutes() {
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/sign-in" element={<SignIn />}></Route>
           <Route path="/sign-up" element={<SignUp />}></Route>
+            <Route path="/patient" element={<Patient />}></Route>
         </Routes>
       </BrowserRouter>
     </>
